ddc1ccb108ad0a293d070e236b13337fa42546bc
Mbarilo <mishapeters7@gmail.com>
2025-11-25 15:43:35 +0200

    initial correct commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+const App = () => {
+  return (
+    <div>
+      <header>
+          <div><h1>LampForum</h1></div>
+          <div><img src="https://i.postimg.cc/bNWnf113/avatar-photo-default-user-icon-600nw-2345549599-removebg-preview-1.png" alt="" /></div>
+      </header>
+      <main>
+        <div>
+          <div>
+            <div>
+              <img src="https://i.postimg.cc/tggq24gq/image-1.png" alt="" />
+              <h2>Profile</h2>
+            </div>
+            <div>
+              <img src="https://i.postimg.cc/NjMSn9RS/image-2.png" alt="" />
+              <h2>Home</h2>
+            </div>
+          </div>
+          <footer>
+            <h3>LampForum, inc.© 2025. All rights reserved.</h3>
+          </footer>
+        </div>
+        <div>
+          <div>
+            <div>
+              <div><img src="https://i.postimg.cc/bNWnf113/avatar-photo-default-user-icon-600nw-2345549599-removebg-preview-1.png" alt=""/></div>
+              <h2>Misha</h2>
+              <div><img src="https://i.postimg.cc/G3YP37hv/image-4.png" alt=""/></div></div>
+            </div>
+            <div>
+              <h2>This is my cat</h2>
+              <img src="https://i.postimg.cc/6pxm54XB/image-3-(1).png" alt="" />
+            </div>
+          </div>
+      </main>
+    </div>
+  );
+};
+
+export default App;
