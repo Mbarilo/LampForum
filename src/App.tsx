f040b5f5f252a6d096ab38ec59c8eb9d7f8c39a9
Mbarilo <mishapeters7@gmail.com>
2025-11-28 23:46:54 +0200

    add styles for main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,33 @@
 import React from 'react';
+import PostCard from './components/PostCard';
+import "./app.css";
 const App = () => {
   return (
     <div>
-      <header>
+      <header className='header'>
           <div><h1>LampForum</h1></div>
           <div><img src="https://i.postimg.cc/bNWnf113/avatar-photo-default-user-icon-600nw-2345549599-removebg-preview-1.png" alt="" /></div>
       </header>
-      <main>
+      <div className='stroke'></div>
+      <main className='main-content'>
         <div>
-          <div>
-            <div>
+          <div className='side-bar'>
+            <div className='profile-link'>
               <img src="https://i.postimg.cc/tggq24gq/image-1.png" alt="" />
               <h2>Profile</h2>
             </div>
-            <div>
+            <div className='home-link'>
               <img src="https://i.postimg.cc/NjMSn9RS/image-2.png" alt="" />
               <h2>Home</h2>
             </div>
           </div>
-          <footer>
-            <h3>LampForum, inc.© 2025. All rights reserved.</h3>
+          <footer className='footer'>
+              <h3>LampForum, inc.© 2025. All rights reserved.</h3>
           </footer>
         </div>
-        <div>
-          <div>
-            <div>
-              <div><img src="https://i.postimg.cc/bNWnf113/avatar-photo-default-user-icon-600nw-2345549599-removebg-preview-1.png" alt=""/></div>
-              <h2>Misha</h2>
-              <div><img src="https://i.postimg.cc/G3YP37hv/image-4.png" alt=""/></div></div>
-            </div>
-            <div>
-              <h2>This is my cat</h2>
-              <img src="https://i.postimg.cc/6pxm54XB/image-3-(1).png" alt="" />
-            </div>
-          </div>
+        <div className='card-content'>
+          <PostCard></PostCard>
+        </div>
       </main>
     </div>
   );
